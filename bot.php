fe608858757793a45f3c47363a020bb6cd90a269
hermanjula3 <103096558+hermanjula3@users.noreply.github.com>
2022-04-15 00:05:48 +0700

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,264 @@
+<?php
+error_reporting(0);
+system("clear");
+
+//color
+$res="\033[0m";
+$hitam="\033[0;30m";
+$abu2="\033[1;30m";
+$putih="\033[0;37m";
+$putih2="\033[1;37m";
+$red="\033[0;31m";
+$red2="\033[1;31m";
+$green="\033[0;32m";
+$green2="\033[1;32m";
+$yellow="\033[0;33m";
+$yellow2="\033[1;33m";
+$blue="\033[0;34m";
+$blue2="\033[ 1;34m";
+$purple="\033[0;35m";
+$purple2="\033[1;35m";
+$lblue="\033[0;36m";
+$lblue2="\033[1;36m";
+
+
+//color
+$hijau = "\33[0;32m";
+$hijau1 = "\33[32;1m";
+$hijau2 = "\e[1;32m";
+$biru = "\33[0;36m";
+$biru1 = "\33[36;1m";
+$biru2 = "\e[1;34m";
+$merah = "\33[31;1m";
+$merah2 = "\e[1;31m";
+$putih2 = "\33[37;1m";
+$putih1 = "\e[1;37m";
+$hitam = "\33[30;1m";
+$kuning = "\33[33;1m";
+$kuning1 = "\33[1;33m";
+$kuning2 = "\e[1;33m";
+$cyan = "\e[0;36m";
+$cyan1 = "\e[1;36m";
+$ungu = "\e[0;35m";
+$ungu2 = "\e[1;35m";
+$abu =	"\e[0;33m";
+$abu1 = "\e[0;37m";
+$abu2 = "\e[1;30m";
+$resu="\033[7m";
+$hitam="\033[0;30m";
+$abu2="\033[1;30m";
+$putih="\033[0;37m";
+$putih2="\033[1;37m";
+$red="\033[0;31m";
+$red2="\033[1;31m";
+$green="\033[0;32m";
+$green2="\033[1;32m";
+$yellow="\033[0;33m";
+$yellow2="\033[1;33m";
+$blue="\033[0;34m";
+$blue2="\033[1;34m";
+$purple="\033[0;35m";
+$purple2="\033[1;35m";
+$lblue="\033[0;36m";
+$lblue2="\033[1;36m";
+
+$Color_Off="\033[0m";      
+$Black="\033[0;30m";      
+$Red= "\033[0;31m";         
+$Green="\033[0;32m";      
+$Yellow="\033[0;33m";       
+$Blue="\033[0;34m";         
+$Purple="\033[0;35m";      
+$Cyan="\033[0;36m";       
+$White="\033[0;37m";       
+$IYellow="\033[0;93m";      
+$IRed="\033[0;91m";         
+$BIRed="\033[1;91m";   
+$On_Cyan="\033[46m";
+$BIWhite="\033[1;97m";     
+$BIBlue="\033[1;94m";      
+$BICyan="\033[1;96m";       
+$BIBlack="\033[1;90m";     
+$BBlack="\033[1;30m";
+$IBlack="\033[0;90m";  
+$On_White="\033[47m";     
+$BIBlue="\033[1;94m";
+$On_IRed="\033[0;101m";
+$On_Red="\033[41m";   
+$On_Blue="\033[44m";
+$On_Green="\033[42m";  
+$IGreen="\033[0;92m";   
+$IYellow="\033[0;93m";
+$Icyan="\033[0;96m";
+//red to yellow shade ↓
+
+$r3="\033[01;38;5;196m";
+$r2="\033[01;38;5;202m";
+$r1="\033[01;38;5;208m";
+$ry="\033[01;38;5;214m";
+$y1="\033[01;38;5;220m";
+$y2="\033[01;38;5;226m";
+$y3="\033[01;38;5;228m";
+/* END COLOR */
+
+echo $baner = "
+$lcyan =================================================================
+$lGreen oooooooooo  ooooooooooo ooooo  oooo  ooooooo  ooooo    ooooooooooo 
+$lGreen  888    888  888    88   888    88 o888   888o 888     88  888  88 
+$lGreen  888oooo88   888ooo8      888  88  888     888 888         888     
+$IYellow  888  88o    888    oo     88888   888o   o888 888      o  888     
+$IYellow o888o  88o8 o888ooo8888     888      88ooo88  o888ooooo88 o888o
+$lcyan =================================================================\n";
+                                                                   
+      echo "\033[1;96m• WELCOME BOSSQUE, HOW ARE YOU THIS TODAY ?\n";
+      echo "\033[1;96m• SEBELUM LANJUT MASUKKAN PASSWORD DULU YAK\n\n";
+      
+
+echo "\033[1;91m ◼ \033[1;92mJANGAN LUPA SUBREK            \r";
+        system("xdg-open https://m.youtube.com/c/mrrevolt");
+        sleep(3);
+        
+echo "\033[1;97m💻️ PRESS \033[1;32mENTER \033[1;37mFOR CONTINUE ⌨️ ⌨️ ⌨️ \033[1;92m";
+$kkk = trim(fgets(STDIN));
+system("clear");
+
+function ban(){
+$SC=1;
+$rev="MR.REVOLT";
+$yt="www.youtube.com/c/MrRevolt";
+$SUPP1 = $cyan."Syukron And All My Friend";
+$SC = $purple2."1.0";
+$SUPP = $cyan."God Almighty and All Who Support Me";
+$sawer = "https://saweria.co/MrRevolt";
+date_default_timezone_set("Asia/Jakarta");
+system("clear");
+echo "\033[01;38;5;228m";
+system("toilet -f wideterm -F border ' C R Y P T O - AFFILIATE (ONLINE) '");
+echo $baner1 = "\033[1;92m oooooooooo  ooooooooooo ooooo  oooo  ooooooo  ooooo    ooooooooooo 
+\033[1;92m  888    888  888    88   888    88 o888   888o 888     88  888  88 
+\033[1;92m  888oooo88   888ooo8      888  88  888     888 888         888     
+\033[1;96m  888  88o    888    oo     88888   888o   o888 888      o  888     
+\033[1;96m o888o  88o8 o888ooo8888     888      88ooo88  o888ooooo88 o888o     
+\033[1;93m===================================================================
+\033[1;96m•SCRIPT INI GRATIS DAN TIDAK UNTUK DI JUAL JADI JANGAN BANYAK MENGELUH
+
+\033[1;37m •THANKS TO       :\033[1;97m $SUPP
+\033[1;37m •SUPPORT         :\033[1;91m $SUPP1
+\033[1;37m •CREATOR         :\033[1;92m $rev
+\033[1;37m •Link YT         :\033[1;93m $yt
+\033[1;37m •Donation        :\033[1;93m $sawer
+\033[1;93m===================================================================\n";
+}
+ban();
+ban();
+if(file_exists("Cookie.txt")){
+$Cookie=file_get_contents("Cookie.txt");}
+if(file_exists("UserAgent.txt")){
+$UserAgent=file_get_contents("UserAgent.txt");
+sleep(1);
+sleep(1);
+}else{
+$Cookie=readline($putih2."INPUT YOUR COOKIE: ");
+file_put_contents("Cookie.txt",$Cookie);
+$UserAgent=readline($putih2."\nINPUT YOUR USER-AGENT: ");
+file_put_contents("UserAgent.txt",$UserAgent);
+ sleep(1);
+}
+function an($str){ 
+    $arr = str_split($str); 
+    foreach ($arr as $az){ 
+    echo $az; 
+    usleep(19999); 
+    }   
+}
+function curl($url, $ua, $data = null){
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_FOLLOWLOCATION => 1,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYPEER => 1,
+            CURLOPT_HTTPHEADER => $ua
+        ));
+        if($data){
+            curl_setopt_array($curl, array(
+                CURLOPT_POSTFIELDS => $data,
+                CURLOPT_POST => 1
+            ));
+       }$result = curl_exec($curl);
+        curl_close($curl);return $result;
+}
+function timer($tmr){
+    $hijaut = "\e[1;32m";
+    $meraht = "\e[1;31m";
+    $kuningt = "\e[1;33m";
+    $putiht = "\e[1;37m";
+    $birut = "\e[1;34m";
+    $ungut = "\e[1;35m";
+    $birumudat = "\e[1;36m";
+    $warna = [$ungut,$birumudat,$hijaut,$ungut,$birut];
+    $timr=time()+$tmr;
+    while(true):
+    echo "\r                           \r";
+    $res = $timr-time();
+    if($res < 1){
+    break;
+    }
+    echo $putiht."[".$warna[array_rand($warna)]."•••".$putiht."] ".gmdate("H:i:s", $res);
+    sleep(1);
+    endwhile;
+    }
+
+function head($UserAgent, $Cookie){
+$ua = [
+    'Host: cryptoaffiliates.store',
+    'User-Agent: '.$UserAgent,
+    'Cookie: '.$Cookie,
+];
+return $ua;
+}ban();
+$url = "https://cryptoaffiliates.store/dashboard";
+$dash = curl($url, head($UserAgent, $Cookie));
+$revolt = explode(' <i class="mdi mdi-chevron-down"></i> </span>',explode('<span class="d-none d-md-inline-block ml-1">',$dash)[1])[0];
+an($yellow2."Welcome Back ".$lblue2."$revolt 😎 !!!".$and."\n\n");
+$v=explode('</span>',explode('<span class="widget-payment-request-product-price">',$dash)[1])[0];
+$balance = explode('</span>',explode('<span class="widget-stats-amount">',$dash)[1])[0];
+$account = explode('</span>',explode('<span class="widget-stats-title">',$dash)[1])[0];$bal=$v;echo $lblue2."💸 Your $account".$putih2."  : ".$y2."$bal ".$putih2."| ".$y3.$balance."\n";$ref = explode('</span>',explode('<span class="widget-stats-title">',$dash)[2])[0];
+$volt=explode('</span>',explode('<span class="widget-stats-amount">',$dash)[2])[0];
+	echo $y2."⚡ $ref".$putih2."  : ".$purple2.$volt."\n";
+an($lblue2."~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".$and."\n");
+an($y3."Ada Dua Metode Yang Dapat Kamu Gunakan: ".$and."\n");
+echo $yellow2."       [".$green2."1".$yellow2."] ".$lblue2."AUTO FAUCET\n";
+echo $yellow2."       [".$green2."2".$yellow2."] ".$r1."WITHDRAW\n";
+echo $green2."Select the Number You Will Run and Enter ".$putih2.": ".$yellow2;
+$menu=trim(fgets(STDIN));
+echo "\n";if($menu==1){
+while(1){
+$url = "https://cryptoaffiliates.store/auto";
+$auto = curl($url, head($UserAgent, $Cookie));
+	$token = explode('"',explode('<input type="hidden" name="token" value="',$auto)[1])[0];
+	$time = explode(',',explode('let timer = ',$auto)[1])[0];
+timer($time);
+$url = "https://cryptoaffiliates.store/auto/verify";
+$data = "token=$token";
+$verify = curl($url, head($UserAgent, $Cookie), $data);
+	$claim = explode("', 'success')</script>",explode("<script> Swal.fire('",$verify)[1])[0];
+	echo $lblue2."[".$green2."✅".$lblue2."] ".$purple2."$claim\n";
+$url = "https://cryptoaffiliates.store/dashboard";
+$ub = curl($url, head($UserAgent, $Cookie));
+$g=explode('</span>',explode('<span class="widget-payment-request-product-price">',$ub)[1])[0];$c=$g;$balantod = explode('</span>',explode('<span class="widget-stats-amount">',$ub)[1])[0];
+	echo $lblue2."[".$yellow2."💸".$lblue2."] ".$y2."Update $account".$putih2."  : ".$y2."$g ".$putih2."| ".$y3.$balantod."\n";
+an($lblue2."~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".$and."\n");
+}}elseif($menu==2){
+echo $yellow2."INDO SUB".$putih2." :".$green2." Penarikan Hanya Bisa Manual,Tidak Dapat Otomatis,Silahkan             Kunjungi Link Dibawah\n\n";
+echo $yellow2."ENG SUB".$putih2."  :".$green2." Withdrawals Can Only Be Manual, Not Automatic,Please Visit            The Link below\n\n";
+echo $yellow2."Link Withdrawal : ".$green2."https://cryptoaffiliates.store/dashboard/withdraw\n\n";
+an($lblue2."~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".$and."\n");
+}
+
+
+
+
+
+
+
